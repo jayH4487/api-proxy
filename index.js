d357047f0c56479bc54214fbae23dfbf789d6172
jayH4487 <59086697+jayH4487@users.noreply.github.com>
2020-09-10 18:56:42 +0100

    restrict to allowed domains

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 require('dotenv').config()
 const express = require("express")
 const fetch = require("node-fetch")
+const cors = require("cors")
+
+
+const allowedDomains = ["localhost:5001"]
+
 
 const app = express()
 
+app.use(cors())
 
 app.get("/api/movie", async (req, res) => {
-    const apiKey = process.env.OMDb_API_KEY
 
-    const searchStr = req.query.s
+    const domain = req.get("host")
+    console.log(domain)
+    console.log(req.headers.origin)
 
-    const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchStr}`
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
-        res.send(data)
-    } catch (error) {
-        console.log(error)
+
+    if (allowedDomains.includes(domain)) {
+        const apiKey = process.env.OMDb_API_KEY
+    
+        const searchStr = req.query.s
+    
+        const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchStr}`
+        try {
+            const response = await fetch(url)
+            const data = await response.json()
+            res.send(data)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        res.send({
+            "Response": "False",
+            "Error": "Movie not found!"
+        })
     }
 
 })
